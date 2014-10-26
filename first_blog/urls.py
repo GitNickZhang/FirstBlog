a0c35d8934df138c82d94f31bfb8982b1d6c4b5f
Nick <317613386@qq.com>
2014-10-26 21:52:59 +0800

    create project in git

---
+++ first_blog/urls.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+import settings
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'first_blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    # 设置引用静态文件的路径
+    url(r'^index/$', 'blog.views.index')
+)
+
