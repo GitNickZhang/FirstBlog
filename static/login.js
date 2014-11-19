3ea5617cfb859401e51cb2c105b471fb67769510
Nick <317613386@qq.com>
2014-11-19 22:21:21 +0800

    fail to use ajax to post because of csrf

--- static/login.js
+++ static/login.js
@@ -13,6 +13,21 @@ $(function(){
 	$('.register').Validform();
 
 	// 注册Ajax
+	var csrftoken = $.cookie('csrftoken');
+
+	function csrfSafeMethod(method) {
+		// these HTTP methods do not require CSRF protection
+		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+	    crossDomain: false, // obviates need for sameOrigin test
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type)) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+
 	$('.btn-register-submit').click(function(){
 		$.ajax({
 			url: '/register/',
